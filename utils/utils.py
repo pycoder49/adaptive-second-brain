204237443bda52553bdd70728ab8068666a58c9f
pycoder49 <aryan-a@outlook.com>
2026-01-14 15:35:04 -0800

    added password utils

---
+++ utils/utils.py
@@ -0,0 +1,16 @@
+from passlib.context import CryptContext
+
+
+pwd_hasher = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    """
+    Hashes a plain text password using bcrypt algorithm.
+    """
+    return pwd_hasher.hash(password)
+
+def verify_password(unhashed_passwrd: str, hashed_password:str) -> bool:
+    """
+    Verifies a plain text password against a hashed password.
+    """
+    return pwd_hasher.verify(unhashed_passwrd, hashed_password)
