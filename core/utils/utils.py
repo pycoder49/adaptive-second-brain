e3a08e0f2827d3c267313dabd51d45a784205ec6
pycoder49 <aryan-a@outlook.com>
2026-01-14 23:23:31 -0800

    small fix

--- core/utils/utils.py
+++ core/utils/utils.py
@@ -3,11 +3,13 @@ from passlib.context import CryptContext
 
 pwd_hasher = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def hash_password(password: str) -> str:
     """
     Hashes a plain text password using bcrypt algorithm.
     """
     return pwd_hasher.hash(password)
+
 
 def verify_password(unhashed_passwrd: str, hashed_password:str) -> bool:
     """
