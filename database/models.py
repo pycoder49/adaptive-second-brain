9e3b3b95b82d6cbe6a1441f8e88238dfeb1ec061
pycoder49 <aryan-a@outlook.com>
2026-01-20 11:35:46 -0800

    added two new tables: conversation and messages to store chat history

--- database/models.py
+++ database/models.py
@@ -17,6 +17,11 @@ class ProcessingStatus(enum.Enum):
     FAILED = "failed"
 
 
+class Role(enum.Enum):
+    USER = "user"
+    AI = "ai"
+
+
 class User(Base):
     # id, email, hased_password
     __tablename__ = "users"
@@ -62,6 +67,27 @@ class Chunk(Base):
 
     # relationships
     document = relationship("Document", back_populates="chunks")
+
+
+class Conversation(Base):
+    # id, user_id (FK), title, created_at
+    __tablename__ = "conversations"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    title = Column(String, nullable=False, default="New Conversation")
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+
+
+class Message(Base):
+    # id, conversation_id(FK), role, content, created_at
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    conversation_id = Column(Integer, ForeignKey("conversations.id"), nullable=False)
+    role = Column(Enum(Role), nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
 
 
 class QueryLog(Base):
