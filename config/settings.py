0526e32c487df1c702fe01b156167171ccd90405
pycoder49 <aryan-a@outlook.com>
2026-01-20 23:27:16 -0800

    added a single source of truth, settings

---
+++ config/settings.py
@@ -0,0 +1,20 @@
+from pydantic import BaseSettings
+from typing import Optional
+
+class Settings(BaseSettings):
+    # database settings
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    # JWT settings
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    class Config:
+        env_file = "./.env"
+    
+settings = Settings()
