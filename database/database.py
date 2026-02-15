89f450486957cca67edebe42b6fc93abee0ebdcc
pycoder49 <aryan-a@outlook.com>
2026-02-15 00:29:44 -0800

    removed load+env from database.py

--- database/database.py
+++ database/database.py
@@ -3,19 +3,16 @@ from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
-from dotenv import load_dotenv
-import os
-load_dotenv()
+from config import settings
 
 
 url = URL.create(
     drivername="postgresql",
-    username=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-    database=os.getenv("DB_NAME")
+    username=settings.DB_USER,
+    password=settings.DB_PASSWORD,
+    host=settings.DB_HOST,
+    port=settings.DB_PORT,
+    database=settings.DB_NAME,
 )
 
 engine = create_engine(url)
