f6028d4c47ae9b6eff99cce9f5351ff2a9916bd8
pycoder49 <aryan-a@outlook.com>
2026-02-17 11:36:45 -0800

    fixed env imports

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from config import settings
+from config.settings import settings
 
 
 url = URL.create(
